af8a997a7aa56931cdafd2bbac736c2dbf30da79
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-06 15:01:39 +0530

    Update main.tf

--- eks-blueprints-terraform/main.tf
+++ eks-blueprints-terraform/main.tf
@@ -124,21 +124,3 @@ module "eks" {
   })
 }
 
-module "ebs_csi_driver_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.20"
-
-  # create_role      = false
-  role_name_prefix = "${module.eks.cluster_name}-ebs-csi-driver-"
-
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-
-  tags = local.tags
-}
