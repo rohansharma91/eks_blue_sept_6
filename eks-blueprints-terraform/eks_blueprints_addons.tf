a8727642c437d190d3433dd3581059bef0dec361
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-06 18:01:02 +0530

    Update eks_blueprints_addons.tf

--- eks-blueprints-terraform/eks_blueprints_addons.tf
+++ eks-blueprints-terraform/eks_blueprints_addons.tf
@@ -34,7 +34,7 @@ module "eks_blueprints_addons" {
 module "eks_blueprints_kubernetes_addons" {
   source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.8.0"
 
-eks_cluster_id = module.eks_blueprints.eks_cluster_id
+eks_cluster_id = module.eks.eks_cluster_id
    
   #region K8s ADDONS
   enable_argocd = false
