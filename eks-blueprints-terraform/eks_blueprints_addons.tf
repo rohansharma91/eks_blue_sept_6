0c4055a0610ac8f5b0a8b2f77d2e33ee8af65bc1
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-06 16:50:40 +0530

    Update eks_blueprints_addons.tf

--- eks-blueprints-terraform/eks_blueprints_addons.tf
+++ eks-blueprints-terraform/eks_blueprints_addons.tf
@@ -24,6 +24,7 @@ module "eks_blueprints_addons" {
 
   enable_aws_load_balancer_controller = true
   enable_metrics_server               = true
+  enable_cluster_autoscaler           = true
 
   enable_argocd = true
   
