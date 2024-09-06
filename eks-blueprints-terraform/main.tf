b196cf747cb3fb9c0cc514abf0d34d261c393f3a
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-06 15:44:19 +0530

    Update main.tf

--- eks-blueprints-terraform/main.tf
+++ eks-blueprints-terraform/main.tf
@@ -86,7 +86,15 @@ module "eks" {
   cluster_version                = "1.30"
   cluster_endpoint_public_access = true
 
-  
+  # EKS Addons
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+    aws-ebs-csi-driver   = {
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+    }
+  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -96,8 +104,8 @@ module "eks" {
       instance_types = ["t2.large"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
 
@@ -124,3 +132,21 @@ module "eks" {
   })
 }
 
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+
+  # create_role      = false
+  role_name_prefix = "${module.eks.cluster_name}-ebs-csi-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = local.tags
+}
