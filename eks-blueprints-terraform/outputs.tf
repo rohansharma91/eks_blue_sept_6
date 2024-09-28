cfa6a5d0f07c6889be2c16f4437d9ce05f7b9ae1
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-28 13:15:23 +0530

    Update outputs.tf

--- eks-blueprints-terraform/outputs.tf
+++ eks-blueprints-terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "eks_blueprints_platform_teams_configure_kubectl" {
+/*output "eks_blueprints_platform_teams_configure_kubectl" {
   description = "Configure kubectl for Platform Team: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}  --role-arn ${module.eks_blueprints_platform_team.iam_role_arn}"
 }
@@ -6,4 +6,4 @@ output "eks_blueprints_platform_teams_configure_kubectl" {
 output "eks_blueprints_dev_teams_configure_kubectl" {
   description = "Configure kubectl for each Dev Application Teams: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = [for team in module.eks_blueprints_dev_teams : "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}  --role-arn ${team.iam_role_arn}"]
-}
+}*/
